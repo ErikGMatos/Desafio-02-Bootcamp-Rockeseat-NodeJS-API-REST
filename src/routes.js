d0fddcb141a3cadab369d8b012be42bbf456edf5
Erik Garces Matos <erik_senac@hotmail.com>
2019-08-21 12:27:23 -0300

    criando load de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello World232' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Erik Matos',
+    email: 'erik_senac@hotmail.com',
+    password_hash: '123456',
+  });
+  res.json(user);
 });
 
 export default routes;
